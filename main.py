9ed5f31e488002a95e12ddeb7bc08dcd7b06b9ee
James Ko <jamesqko@gmail.com>
2018-07-02 14:57:00 -0400

    Change url, switch to requests

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
 
-import aiohttp
 import asyncio
 import bs4
+import requests
 from urllib.parse import urljoin
 
 async def main():
-    corpus = await scrape()
+    corpus = scrape()
     print(len(corpus))
     print(corpus[1])
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
-
-async def scrape():
+def scrape():
     # scrape corpus from apstudynotes
 
-    url = 'https://www.apstudynotes.org/essays/'
-    async with aiohttp.ClientSession() as sess:
-        html = await fetch(sess, url)
+    url = 'http://web.archive.org/web/20160207232714/www.apstudynotes.org/essays'
+    # For some reason aiohttp does not work with web.archive.org, use requests instead
+    html = requests.get(url).text
 
-        bs = bs4.BeautifulSoup(html, 'html.parser')
-        pattern = '.entry .heading h3 a'
-        anchors = bs.select(pattern)
-        page_urls = [urljoin(url, a['href']) for a in anchors]
+    bs = bs4.BeautifulSoup(html, 'html.parser')
+    pattern = '.entry .heading h3 a'
+    anchors = bs.select(pattern)
+    page_urls = [urljoin(url, a['href']) for a in anchors]
 
-        return await asyncio.gather(*[scrape_page(sess, pg) for pg in page_urls])
+    return [scrape_page(pg) for pg in page_urls]
 
-async def scrape_page(sess, url):
-    html = await fetch(sess, url)
+def scrape_page(url):
+    html = requests.get(url).text
     bs = bs4.BeautifulSoup(html, 'html.parser')
     pattern = '.body p'
     paras = bs.select(pattern)
